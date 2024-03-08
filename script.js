ef1aa2450549bc2fbdaf07c6031a238f33e2f616
Your Name <om.pawar1512@gamil.com>
2024-03-08 15:59:30 +0530

    version (1.0.14) coursor animation completed

--- script.js
+++ script.js
@@ -2,33 +2,41 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
-function moving_courser() {
-let main_about_box = document.querySelector("#section2");
-let hey_btn = document.querySelector("#hey");
-  main_about_box.addEventListener("mouseenter", function () {
-    gsap.to(hey_btn, {
-      scale: 1,
-      opacity: 1,
-    });
-  });
-  main_about_box.addEventListener("mouseleave", function () {
-    gsap.to(hey_btn, {
-      scale: 0,
-      opacity: 0,
-    });
-  });
-  main_about_box.addEventListener("mousemove", function (dets) {
-    gsap.to(hey_btn, {
-      left: (dets.x - 80),
-      top: (dets.y - 80)
-    });
-  });
 
-}
-moving_courser();
+
+
+// function moving_courser() {
+// let main_about_box = document.querySelector("#main_about_box");
+// let hey_btn = document.querySelector("#hey");
+//   // main_about_box.addEventListener("mouseenter", function () {
+//   //   gsap.to(hey_btn, {
+//   //     scale: 1,
+//   //     opacity: 1,
+//   //     delay: 1,
+//   //     transition:.3
+//   //   });
+//   // });
+//   // main_about_box.addEventListener("mouseout", function () {
+//   //   gsap.to(hey_btn, {
+//   //     scale: 0,
+//   //     opacity: 0,
+//   //     delay: 1,
+//   //     transition:.3
+//   //   });
+//   // });
+//   main_about_box.addEventListener("mousemove", function (dets) {
+//     let higth = screen.height
+//     gsap.to(hey_btn, {
+//       left: (dets.x -80 ),
+//       top: (dets.y + higth)
+//     });
+//     console.log(dets.y + higth)
+//   });
+
+// }
+// moving_courser();
 
 let home_bg_img = document.getElementById("home_bg_img");
-let All_info_1 = document.getElementById("All_info_1");
 let info_box = document.getElementsByClassName("info_box");
 
 function loading_animation() {
@@ -58,4 +66,60 @@ function loading_animation() {
   });
 }
 loading_animation();
+
+
+document.addEventListener("mousemove" , function(dets){
+  gsap.to("#coursor",{
+    left: dets.x,
+    top:dets.y,
+  })
+})
+
+document.querySelector("#main_about_box").addEventListener("mouseenter" ,function(){
+  gsap.to("#coursor",{
+    transform: 'translate(-50% ,-50%) scale(0)'
+  })
+})
+document.querySelector("#main_about_box").addEventListener("mouseleave" ,function(){
+  gsap.to("#coursor",{
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
+document.querySelector(".main_projects_box").addEventListener("mouseenter" ,function(){
+  gsap.to("#coursor",{
+
+    transform: 'translate(-50% ,-50%) scale(0)'
+  })
+})
+document.querySelector(".main_projects_box").addEventListener("mouseleave" ,function(){
+  gsap.to("#coursor",{
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
+
+document.querySelector("#card_box").addEventListener("mouseenter" ,function(){
+  gsap.to("#coursor",{
+    scale:3,
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
+document.querySelector("#card_box").addEventListener("mouseleave" ,function(){
+  gsap.to("#coursor",{
+    scale:1,
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
+document.querySelector("footer>div>svg").addEventListener("mouseenter" ,function(){
+  gsap.to("#coursor",{
+    scale:3,
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
+
+document.querySelector("footer>div>svg").addEventListener("mouseleave" ,function(){
+  gsap.to("#coursor",{
+    scale:1,
+    transform: 'translate(-50% ,-50%) scale(1)'
+  })
+})
 
